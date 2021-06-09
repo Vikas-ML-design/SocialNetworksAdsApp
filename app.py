c281a4063e2884f3e476af97df3e2f4d958ae6d9
Vikas-ML-design <85618650+Vikas-ML-design@users.noreply.github.com>
2021-06-09 17:06:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = tf.keras.models.load_model(open('saved_model.pb', 'rb'))
+scFeatures = pickle.load(open('CustomerCategoryFeatureMod.ft','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    age = float(request.form['age'])
+    sal = float(request.form['sal'])
+    stdFeatures = scFeatures.transform(age)
+    predLabel = model.predict_classes(stdFeatures)      
+
+    return render_template('index.html', prediction_text='Given customer is a {} customer".format("Good" if predLabel[0][0] == 1 else "Bad"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
